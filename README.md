05c2e2a38aae681d798c5ed0a5d6f0e1df21874f
rogelio-lopez <rogejlopez@gmail.com>
2022-05-27 06:26:42 -0400

    finished basic styling, code cleanup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Sorting-Algorithms
 Sorting algorithms visualizer built in HTML, CSS, JS
+
+
+Todo:
+- Connect bubble sort algorithm to html/css
+    - Switch from using numbers array to Object array
+    - Sorting ElementObj Objects
--- app.js
+++ app.js
@@ -7,33 +7,40 @@ const elementObj = {
 }
 
 
-// Random number array 
-const elAmount = document.querySelector('.el-amount input');
+// Creating dynamic random ElementObj array
+const elAmount = document.querySelector('.quantity input');
 const elContainer = document.querySelector('.el-container');
 
-elAmount.addEventListener('change', (e) => {
-  // create array then render
-  elContainer.innerHTML = '';
-  let elArray = [];
-  
-  for (let i = 0; i < e.target.value; i++){
-    let random = Math.floor(Math.random() * 90) + 10;
-
-    elArray.push(createHtmlElement(random));
-  }
-  displayElementArray(elArray);
-}); 
-
-const createHtmlElement = (rand) => {
+const createHtmlElementObj = (rand) => {
   let li = document.createElement("li");
-
   li.classList.add("element");
-  li.classList.add("inner-shadow-sm");
+  li.classList.add("shadow-sm");
   li.style.height = `${rand * 4}px`;
 
   return li;
 }
 
-const displayElementArray = (arr) => {
+const displayElementObjArray = (arr) => {
   arr.forEach(el => elContainer.appendChild(el));
 }
+
+const createElementObjArray = (quantity) => {
+  elContainer.innerHTML = '';
+  let elArray = [];
+  
+  for (let i = 0; i < quantity; i++){
+    let random = Math.floor(Math.random() * 90) + 10;
+    elArray.push(createHtmlElementObj(random));
+  }
+  displayElementObjArray(elArray);
+}
+
+elAmount.addEventListener('change', (e) => {
+  createElementObjArray(e.target.value);
+}); 
+
+
+
+
+// Create array on page load
+createElementObjArray(50);
