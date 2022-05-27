98c77247e8af7ddbbfa749c96f1fb5e66ba81744
rogelio-lopez <rogejlopez@gmail.com>
2022-05-27 19:04:02 -0400

    creating random obj instead of numbered array

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ Sorting algorithms visualizer built in HTML, CSS, JS
 
 Todo:
 - Connect bubble sort algorithm to html/css
-    - Switch from using numbers array to Object array
     - Sorting ElementObj Objects
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const elementObj = {
-  key: '', // key = array position
+  key: 0, // key = array position
   value: 0, 
   selected: false,
   moved: false,
@@ -11,6 +11,7 @@ const elementObj = {
 const elAmount = document.querySelector('.quantity input');
 const elContainer = document.querySelector('.el-container');
 
+// Creates li with where height = (randomNum * 4)px
 const createHtmlElementObj = (rand) => {
   let li = document.createElement("li");
   li.classList.add("element");
@@ -25,9 +26,16 @@ const createElementObjArray = (quantity) => {
   let elArray = [];
   
   for (let i = 0; i < quantity; i++){
-    let random = Math.floor(Math.random() * 90) + 10;
-    elArray.push(createHtmlElementObj(random));
+
+    //Creates new elementObj, adds info, inserts into obj array
+    const obj = Object.create(elementObj);
+    obj.key = i;
+    obj.value = Math.floor(Math.random() * 90) + 10;
+    obj.htmlElement = createHtmlElementObj(obj.value);
+
+    elArray.push(obj);
   }
+
   displayElementObjArray(elArray);
 }
 
@@ -36,7 +44,7 @@ const createElementObjArray = (quantity) => {
  * Display changes in sorting (everything stays the same except for 2)
  */
 const displayElementObjArray = (arr) => {
-  arr.forEach(el => elContainer.appendChild(el));
+  arr.forEach(el => elContainer.appendChild(el.htmlElement));
 }
 
 elAmount.addEventListener('change', (e) => {
